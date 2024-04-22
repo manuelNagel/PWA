99e1e89ab6d4fa48f41104ab4c127c6c818e7ba0
Juan Manuel Nagel Sabio <juanmanage@gmail.com>
2024-04-22 00:10:34 -0300

    validación

--- tp-react/src/components/ToDoForm.js
+++ tp-react/src/components/ToDoForm.js
@@ -2,15 +2,21 @@ import React,{useState} from 'react'
 
 export const ToDoForm = ({addTodo}) => {
     const [value,setValue] = useState("")
+    const [mensaje,setMensaje] = useState("¿Cuál es la tarea a agregar");
     const handleSubmit = e =>{
         e.preventDefault();
-        addTodo(value);
-        setValue("")
+        if (value.trim() !== "") {
+            addTodo(value);
+            setValue("");
+            setMensaje ("¿Cuál es la tarea a agregar");
+        } else {
+            setMensaje("Ingrese un texto Por favor");
+        }
     }
   return (
     <form className= 'ToDoForm' onSubmit={handleSubmit}> 
         <input type='text' className='todo-input' value={value}
-        placeholder='¿Cuál es la tarea a agregar?'
+        placeholder={mensaje}
         onChange={(e)=>setValue(e.target.value)}/>
         <button type='submit' className='todo-btn'> Agregar tarea </button>
     </form>
